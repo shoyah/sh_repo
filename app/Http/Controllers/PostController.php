c01503daf201718be3b27545e817b9373ccd05bc
shoyah <rio0901dog@outlook.jp>
2022-02-02 16:34:44 +0900

    修正中

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,17 +3,17 @@
 namespace App\Http\Controllers;
 
 use App\Post;
- dev_basis02
 
 
- master
+
+ 
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function index(Post $post)
     {
- dev_basis02
+
         return view('posts/index')->with(['posts' => $post->get()]);  
     }
 }
@@ -22,4 +22,4 @@ class PostController extends Controller
     return $post->get();
     }
 }
- master
+
