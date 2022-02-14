4250e78db5c449459e31ae570b62c24613a0ea86
shoyah <rio0901dog@outlook.jp>
2022-02-14 10:12:34 +0900

    カリキュラム09_1の内容

--- database/migrations/2022_02_14_100533_add_image_column_to_posts_table.php
+++ database/migrations/2022_02_14_100533_add_image_column_to_posts_table.php
@@ -4,18 +4,17 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePostsTable extends Migration
+class AddImageColumnToPostsTable extends Migration
 {
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
+       public function up()
     {
-        Schema::create('posts', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->timestamps();
+        Schema::table('posts', function (Blueprint $table) {
+            $table->string('image', 100)->nullable();
         });
     }
 
@@ -26,6 +25,8 @@ class CreatePostsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('posts');
+        Schema::table('posts', function (Blueprint $table) {
+            //
+        });
     }
 }
